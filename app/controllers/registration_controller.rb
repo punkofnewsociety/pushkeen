f19ff8b63c5b9bd9188004a29311a116c5c9a988
punkofnewsociety <punkofnewsociety@gmail.com>
2015-12-27 22:08:35 +0300

    add RENDER

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -1,7 +1,13 @@
 class RegistrationController < ApplicationController
-  def index
+  def index ()
     p "123"
+    a = @token
+    p a
+    b = params[:token]
+    p b
     p params[:question]
     p params[:token]
+    render json: {answer: 'мглою'}
   end
+  
 end
