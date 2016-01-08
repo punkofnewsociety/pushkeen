a9e3bd26548a5291fa1fd2569a1077531ac10edf
punkofnewsociety <punkofnewsociety@gmail.com>
2016-01-08 07:17:49 +0300

    make it bettter

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -3,8 +3,6 @@ class RegistrationController < ApplicationController
   skip_before_filter  :verify_authenticity_token
   $taken = 0
   def index    
-    aaa = params[:question]
-    abc = params[:token]
     puts "#$taken"
   end
   
@@ -23,30 +21,50 @@ class RegistrationController < ApplicationController
   @question = params[:question]
   @id = params[:id]
   @level = params[:level]
-  render nothing:true
+  #render nothing:true
   
   case @level
-  when 1
+  when '1'
     self.first
-  when 2
-    puts "HHHHHHHHHHTTTTTTTTTTIIIIII"
+  when '2'
+    self.second
+  when '3'
+    self.third
   end
+    
   puts @question
   
   puts @answer
-  
+  puts "123"
   uri = URI("http://pushkin-contest.ror.by/quiz")
   parameters = {
     answer: @answer,
     token: 'dc574bf8a22d1adceb04e8f6c4fefc5d',
     task_id:  @id
   }
-  
-  Net::HTTP.post_form(uri, parameters)
+  render json: {answer: @answer} 
+  #Net::HTTP.post_form(uri, parameters)
   end
   
   def first
     @answer = Title.find_by_id(Str.find_by("text LIKE ?", "%#{@question}%").titleid).name
   end
   
+  def second
+    st = @question.split(' %WORD% ')
+    p = Str.find_by("text LIKE ?", "%#{st[0]}%" "%#{st[1]}%").text.gsub(/#{st[0]}(.*?)#{st[1]}/){@answer = $1.strip}
+  end
+  
+  def third
+    ch = @question.split('\n')
+    st = ch[0].to_s.split(' %WORD% ')
+    @question=ch[0]
+    self.second
+    firstword=@answer
+    @question=ch[1]
+    self.second
+    secondword=@answer
+    @answer=firstword+','+secondword
+
+  end
 end     
