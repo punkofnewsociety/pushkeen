36d9933f672a7c9861dca442385a37143455c229
punkofnewsociety <punkofnewsociety@gmail.com>
2016-01-10 20:31:31 +0300

    tired

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   post '/quiz' => 'registration#play'
   get '/registration' =>  'registration#index'
   post '/registration' =>  'registration#new'
-  get '/quiz' => 'registration#answer'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
