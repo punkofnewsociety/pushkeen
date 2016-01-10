72a9984345c8097aafa7be900f91fe0f84a31f17
punkofnewsociety <punkofnewsociety@gmail.com>
2016-01-10 19:59:41 +0300

    topost

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -33,6 +33,8 @@ class RegistrationController < ApplicationController
   when '4'
     self.fourth
   end
+  puts @question
+  puts @answer
   uri = URI("http://pushkin-contest.ror.by/quiz")
   parameters = {
     answer: @answer,
