49bd751060bdc81f73bfdb4933d64c1bc81cc6ec
punkofnewsociety <punkofnewsociety@gmail.com>
2016-01-12 11:05:33 +0300

    lets try

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -22,16 +22,16 @@ class RegistrationController < ApplicationController
   @question = params[:question]
   @id = params[:id]
   @level = params[:level]
-  render nothing:true
+  #render nothing:true
   
   case @level
-  when 1
+  when '1'
     self.first
-  when 2
+  when '2'
     self.second
-  when 3
+  when '3'
     self.third
-  when 4
+  when '4'
     self.fourth
   end
   puts @question
@@ -43,8 +43,8 @@ class RegistrationController < ApplicationController
     token: 'dc574bf8a22d1adceb04e8f6c4fefc5d',
     task_id:  @id
   }
-  #render json: {answer: @answer} 
-  Net::HTTP.post_form(uri, poparameters)
+  render json: {answer: @answer} 
+  #Net::HTTP.post_form(uri, poparameters)
   end
   
   def first
@@ -52,16 +52,47 @@ class RegistrationController < ApplicationController
   end
   
   def second
+    if @question.index("%") == 0 then
+    st = @question.split('%WORD%')
+    puts st[1]
+    
+    p = Str.find_by("text LIKE ?", "%#{st[1]}%").text.gsub(/(.*?)#{st[1]}/){@answer = $1.strip}
+    i=0
+    a=String.new
+      while      !!@answer.reverse![i].downcase! do
+       a = a + @answer[i]
+       i+=1
+      end
+    @answer=a
+    else
     st = @question.split(' %WORD% ')
-    p = Str.find_by("text LIKE ?", "%#{st[0]}%" "%#{st[1]}%").text.gsub(/#{st[0]}(.*?)#{st[1]}/){@answer = $1.strip}
+    #p = Str.find_by("text LIKE ?", "%#{st[0]}%" "%#{st[1]}%").text.gsub(/#{st[0]}(.*?)#{st[1]}/){@answer = $1.strip}
+    p = Str.find_by("text LIKE ?", "%#{st[0]}%" "%#{st[1]}%").text
+    p st[0]
+    p st[1]
+     leftstr=p.partition(st[0]+' ')
+     xxx=leftstr.delete("")
+     p leftstr
+    puts "123"
+     rightstr=p.partition(' '+st[1])
+     p rightstr
+    top=rightstr[0].delete(leftstr[0])
+    p top
+    @answer=top.to_s
+    
+    end
   end
   
   def third
     ch = @question.split('\n')
+    puts ch[0]
+    puts "HUI"
+    puts ch[1]
     @question=ch[0]
     self.second
     firstword=@answer
     @question=ch[1]
+    puts @answer
     self.second
     secondword=@answer
     @answer=firstword+', '+secondword
